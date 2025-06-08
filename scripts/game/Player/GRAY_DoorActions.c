ebbf8a765c0f4f7e9c3d3f8620eb4c2e80cf7fc3
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2025-06-08 10:22:06 -0400

    Add files via upload

---
+++ scripts/game/Player/GRAY_DoorActions.c
@@ -0,0 +1,69 @@
+class GRAY_AdjustDoorAction : SCR_AdjustSignalAction
+{
+	protected static const float STEP = 0.05;
+	protected DoorComponent m_doorComponent;
+	protected float m_lastStep;
+	
+	protected override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
+	{
+		super.Init(pOwnerEntity, pManagerComponent);
+		
+		m_doorComponent = DoorComponent.Cast(pOwnerEntity.FindComponent(DoorComponent));
+		m_fAdjustmentStep = STEP;
+	}
+	
+	override protected bool OnLoadActionData(ScriptBitReader reader)
+	{
+		bool result = super.OnLoadActionData(reader);
+		if(Replication.IsServer())
+			HandleAdjust();
+		return result;
+	}
+	
+	override void OnActionStart(IEntity pUserEntity)
+	{
+		super.OnActionStart(pUserEntity);
+		
+		RplComponent rpl = RplComponent.Cast(pUserEntity.FindComponent(RplComponent));
+		if(!rpl || !rpl.IsOwner())
+			return;
+		
+		float control = m_doorComponent.GetControlValue();
+		SetSignalValue(control);
+		m_fTargetValue = control;
+		m_lastStep = control;
+	}
+	
+	protected void HandleAdjust()
+	{
+		if(!Replication.IsServer())
+			return;
+		
+		float currentStep = SCR_GetCurrentValue();
+		if(currentStep == m_lastStep)
+			return;
+		
+		m_lastStep = currentStep;
+		m_doorComponent.SetControlValue(currentStep);
+	}
+}
+
+class GRAY_OpenDoorAIAction : ScriptedUserAction
+{
+	protected DoorComponent m_doorComponent;
+	
+	protected override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
+	{
+		super.Init(pOwnerEntity, pManagerComponent);
+		
+		m_doorComponent = DoorComponent.Cast(pOwnerEntity.FindComponent(DoorComponent));
+	}
+	
+	override bool CanBePerformedScript(IEntity user)
+	{
+		if (!EntityUtils.IsPlayer(user))
+			m_doorComponent.SetControlValue(1);
+
+		return true;
+	}
+}
